38f25ff0ec6d4d6d237abbbc7117e252bde61a20
Andrew Nagy <andrew.nagy@the159.com>
2017-03-30 12:17:51 -0700

    FREEPBX-14434 Clicking REST API Report crashes GUI

--- views/log_report.php
+++ views/log_report.php
@@ -9,65 +9,64 @@ $html .= '<link href="http://alexgorbatchev.com/pub/sh/current/styles/shThemeDef
 $html .= '<script src="http://alexgorbatchev.com/pub/sh/current/scripts/shCore.js" type="text/javascript"></script>';
 $html .= '<script src="http://alexgorbatchev.com/pub/sh/current/scripts/shAutoloader.js" type="text/javascript"></script>';
 
-		
 if (!$logs) {
 	$html .= _('No Data Found!');
 	echo $html;
-	exit();
+	return;
 }
 
 $table = new CI_Table;
 //set some classes for more controll in this table
 $table->set_template(array(
-					'table_open' => '<table id="log_table_parent">', 
+					'table_open' => '<table id="log_table_parent">',
 					'row_start' => '<tr class="hover">',
 					));
 $table->set_heading('',
-				_('ID'), 
-				_('Time') . '<span class="localtime" data-done-text="' 
-							. _('Localized') . '"> (' 
-							. _('Localizing...') 
-							. ')</span>', 
-				_('Token'), 
-				_('Signature'), 
-				_('IP'), 
+				_('ID'),
+				_('Time') . '<span class="localtime" data-done-text="'
+							. _('Localized') . '"> ('
+							. _('Localizing...')
+							. ')</span>',
+				_('Token'),
+				_('Signature'),
+				_('IP'),
 				_('Server'));
 
 //iterate over log entires
 foreach ($logs as $l) {
 	$log = isset($log) ? false : true;
-	
+
 	$table->add_row(
 					'<span class="show_event" data-id="' . $l['id'] . '">+</span>',
-					$l['id'], 
-					'<span class="log_raw_time" data-date="' . $l['time'] . '">' 
-						. date('Y-m-d H:i:s', $l['time']) 
+					$l['id'],
+					'<span class="log_raw_time" data-date="' . $l['time'] . '">'
+						. date('Y-m-d H:i:s', $l['time'])
 						. '</span>',
 					'<span class="hashtr">' . $l['token'] . '</span>',
 					'<span class="hashtr">' . $l['signature'] . '</span>',
 					$l['ip'],
 					'<span class="hashtr">' . $l['server'] . '</span>'
 					);
-					
+
 	//iterate over events, if we have any
 	if (isset($l['events']) && $l['events']) {
 		$eTable = new CI_Table;
 		$eTable->set_template(array('table_open' => '<table id="log_table_events">'));
 		foreach ($l['events'] as $e) {
 			$eTable->set_heading(
-					_('Time'), 
-					_('Event'), 
+					_('Time'),
+					_('Event'),
 					_('Data') . '<span class="datahelp"> ('._('click on data to expand').')</span>'
 			);
 			$eTable->add_row(
-						'<span class="log_raw_time" data-date="' . $e['time'] . '">' 
-							. date('Y-m-d H:i:s', $e['time']) 
+						'<span class="log_raw_time" data-date="' . $e['time'] . '">'
+							. date('Y-m-d H:i:s', $e['time'])
 							. '</span>',
-						'<span class="log_event" title="' . $e['trigger'] . '">' 
-							. $e['event'] 
+						'<span class="log_event" title="' . $e['trigger'] . '">'
+							. $e['event']
 							. '</span>',
 						//'<div class="event_data"><pre class="brush: php">' . var_export($e['data'], true) . '</pre></div>'
-						'<div class="event_data" data-state="closed"><pre>' 
+						'<div class="event_data" data-state="closed"><pre>'
 						. print_r($e['data'], true) . '</pre></div>'
 			);
 		}
@@ -78,9 +77,9 @@ foreach ($logs as $l) {
 $html .= $table->generate();
 
 $html .= '<script type="text/javascript">'
-		. 'SyntaxHighlighter.autoloader("php http://alexgorbatchev.com/pub/sh/current/scripts/shBrushPhp.js");' 
+		. 'SyntaxHighlighter.autoloader("php http://alexgorbatchev.com/pub/sh/current/scripts/shBrushPhp.js");'
 		. 'SyntaxHighlighter.defaults["toolbar"] = false;'
 		. 'SyntaxHighlighter.defaults["gutter"] = false;'
 		. 'SyntaxHighlighter.all();</script>';
-echo $html;	
+echo $html;
 ?>
